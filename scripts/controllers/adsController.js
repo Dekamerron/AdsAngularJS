ba1fd91daba8f3a73454bc2cfb07b345daa0eb52
Dekameron <mbtakanov@yahoo.com>
2015-01-03 00:47:12 +0200

    Update

--- scripts/controllers/adsController.js
+++ scripts/controllers/adsController.js
@@ -4,7 +4,25 @@ app.controller('AdsController', ['$scope', '$location', 'adsServices', function 
 			$scope.data = data;
 		},
 		function (error, status, headers, config) {
-			// console.log(status, error);
+			console.log(status, error);
+		}
+	);
+
+	adsServices.getAllCategories(
+		function (data, status, headers, config) {
+			$scope.categories = data;
+		},
+		function (error, status, headers, config) {
+			console.log(status, error);
+		}
+	);
+
+	adsServices.getAllTowns(
+		function (data, status, headers, config) {
+			$scope.towns = data;
+		},
+		function (error, status, headers, config) {
+			console.log(status, error);
 		}
 	);
 
@@ -18,6 +36,18 @@ app.controller('AdsController', ['$scope', '$location', 'adsServices', function 
 
 	$scope.back = function () {
 		$location.path('/');
+	};
+
+	$scope.categoryIndex = -1;
+	$scope.categoryClicked = function (category, $index) {
+	    $scope.categoryIndex = $index;
+	    $scope.categoryId = category.id;
+	};
+
+	$scope.townIndex = -1;
+	$scope.townClicked = function (town, $index) {
+	    $scope.townIndex = $index;
+	    $scope.townId = town.id;
 	};
 
 }]);
--- scripts/services/adsServices.js
+++ scripts/services/adsServices.js
@@ -2,7 +2,7 @@ app.factory('adsServices', function ($resource, $http) {
 	function getAllAds (success, error) {
 		$http({
 			method: 'GET',
-			url: 'http://softuni-ads.azurewebsites.net/api/ads'
+			url: 'http://softuni-ads.azurewebsites.net/api/ads?PageSize=20&Startpage=1'
 		})
 		.success(function (data, status, headers, config) {
 			success(data, status, headers(), config);
@@ -10,9 +10,37 @@ app.factory('adsServices', function ($resource, $http) {
 		.error(function (data, status, headers, config) {
 			error(data, status, headers, config);
 		});
-	}
+	};
+
+	function getAllCategories (success, error) {
+		$http({
+			method: 'GET',
+			url: 'http://softuni-ads.azurewebsites.net/api/categories'
+		})
+		.success(function (data, status, headers, config) {
+			success(data, status, headers(), config);
+		})
+		.error(function (data, status, headers, config) {
+			error(data, status, headers, config);
+		});
+	};
+
+	function getAllTowns (success, error) {
+		$http({
+			method: 'GET',
+			url: 'http://softuni-ads.azurewebsites.net/api/towns'
+		})
+		.success(function (data, status, headers, config) {
+			success(data, status, headers(), config);
+		})
+		.error(function (data, status, headers, config) {
+			error(data, status, headers, config);
+		});
+	};
 
 	return {
-		getAllAds: getAllAds
+		getAllAds: getAllAds,
+		getAllCategories: getAllCategories,
+		getAllTowns: getAllTowns
 	}
 });
